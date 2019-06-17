796aab3195a68db7e32c2f047644a370ba0f71fc
Kuba <kuba.pod@gmail.com>
2019-06-17 14:34:00 +0200

    update: paclet info

--- PreciseTracking/PacletInfo.m
+++ PreciseTracking/PacletInfo.m
@@ -1,6 +1,6 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Name -> "PreciseTracking", Version -> "0.0.1", 
- MathematicaVersion -> "10.4+", Description -> 
+Paclet[Name -> "PreciseTracking", Version -> "0.1.0",
+ MathematicaVersion -> "11.3+", Description ->
   "Fine grained Dynamic updating for Mathematica", 
  Creator -> "Kuba Podkalicki (kuba.pod@gmail.com)", 
- Extensions -> {{"Kernel", Context -> "PreciseTracking`"}, {"FrontEnd"}}]
+ Extensions -> {{"Kernel", Context -> "PreciseTracking`"}}]
